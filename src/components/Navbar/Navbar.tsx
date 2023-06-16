d2175bc4ffb9aae8da134e88f4026f595e623861
Maksym-Holovachuk <mgolovachuk@gmail.com>
2023-06-16 17:09:20 +0300

    change Footer, added link to linkedin

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { NavMenuType } from "../../types/index";
+import { NavMenuType } from "@/types/index";
 import Link from "next/link";
 
 import pages from "@/db/pages/pages.json";
