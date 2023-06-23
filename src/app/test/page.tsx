a64668afcee4f68eb852101c4858268d68b534fe
Maksym-Holovachuk <mgolovachuk@gmail.com>
2023-06-23 21:38:46 +0300

    add test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface ITestPageProps {}
+
+const TestPage: FC<ITestPageProps> = () => {
+  return (
+    <>
+      <div>TestPage</div>
+    </>
+  );
+};
+
+export default TestPage;
